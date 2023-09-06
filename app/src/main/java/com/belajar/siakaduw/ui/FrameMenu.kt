08df7315cbd489e004a1f3c2a4eee815cc538ce5
Fajar Pahmi Padilah <pahmipadilah76@gmail.com>
2023-09-06 22:51:38 +0700

    create frame menu

---
+++ app/src/main/java/com/belajar/siakaduw/ui/FrameMenu.kt
@@ -0,0 +1,12 @@
+package com.belajar.siakaduw.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.belajar.siakaduw.R
+
+class FrameMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_menu)
+    }
+}
